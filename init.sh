703e3e54d6bd925696ad88532e22fdd5420c1024
Peter Booker <mail@peterbooker.com>
2018-10-30 09:46:21 +0000

    added defaults to env vars

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@
 
 cd /var/www/html
 if ! $(wp core is-installed --allow-root); then
-    wp core download --version=${WP_VERSION} --force --allow-root \
-    && wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=database --force --allow-root \
-    && wp core install --url=localhost --title=WordPress --admin_user=${WP_USER} --admin_password=${WP_PASS} --admin_email=${WP_EMAIL} --allow-root \
+    wp core download --version=${WP_VERSION:-latest} --force --allow-root \
+    && wp core config --dbname=${MYSQL_DATABASE:-wordpress} --dbuser=${MYSQL_USER:-wordpress} --dbpass=${MYSQL_PASSWORD:-password} --dbhost=database --force --allow-root \
+    && wp core install --url=localhost --title=WordPress --admin_user=${WP_USER:-user} --admin_password=${WP_PASS:-password} --admin_email=${WP_EMAIL:-user@domain.com} --allow-root \
     && wp rewrite structure "/%postname%/" --allow-root \
     && wp site empty --allow-root \
     && wp widget delete search-2 recent-posts-2 recent-comments-2 archives-2 categories-2 meta-2 --allow-root
