231873629c076c8a4fb523c35771b5f90d802089
Peter Booker <mail@peterbooker.com>
2018-11-10 16:46:40 +0000

    simplify adminer install and correct wp installed check

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /var/www/html
-if ! $(wp core is-installed --allow-root); then
+if $(wp core is-installed); then
     wp core download --version=${WP_VERSION:-latest} --force --allow-root \
     && wp core config --dbname=${MYSQL_DATABASE:-wordpress} --dbuser=${MYSQL_USER:-wordpress} --dbpass=${MYSQL_PASSWORD:-password} --dbhost=database --force --allow-root \
     && wp core install --url=localhost --title=WordPress --admin_user=${WP_USER:-user} --admin_password=${WP_PASS:-password} --admin_email=${WP_EMAIL:-user@domain.com} --allow-root \
@@ -11,9 +11,8 @@ if ! $(wp core is-installed --allow-root); then
 fi
 
 cd /var/www/adminer
-if [ ! -e index.php ]; then
-    wget https://github.com/vrana/adminer/releases/download/v4.6.3/adminer-4.6.3.php \
-    && mv adminer-4.6.3.php ./adminer.php
+if [ ! -e adminer.php ]; then
+    wget https://github.com/vrana/adminer/releases/download/v4.6.3/adminer-4.6.3.php -O adminer.php
 fi
 
 exec "$@"
